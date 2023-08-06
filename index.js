4f8e13ff879818f949c55b25abef1a40b0732bf4
varunchithiraala <varuncvk13@gmail.com>
2023-08-06 03:58:10 +0000

    Super Over League App Solution

---
+++ index.js
@@ -0,0 +1,12 @@
+const element = (
+//   Write your code here.
+   <div className="super-over-league-container">
+        <h1 className="super-over-league-heading">Super Over League</h1>
+        <div>
+            <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" className="super-over-league-img" />
+            <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" className="super-over-league-img" />
+        </div>
+   </div>
+);
+
+ReactDOM.render(element, document.getElementById('root'))
